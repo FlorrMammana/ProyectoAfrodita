95f13950da01ce84edc93dcd140ab774cc8162eb
florrmammana <floramammana1994@gmail.com>
2021-12-09 23:26:37 -0300

    navbar

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import NavBar from "./components/NavBar"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+    return (
+    <NavBar/>
+    )
+} 
+
+export default App
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="nav">
+                <li className="nav-link active" aria-current="page">Active</li>
+                <li className="nav-link">Link</li>
+                <li className="nav-link">Link</li>
+                <li className="nav-link ">Disabled</li>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
