937b8b2c1a1755e770d4055e0d04423ade51954a
florrmammana <floramammana1994@gmail.com>
2022-01-25 20:20:19 -0300

    coleccion nueva firebase

--- src/components/ItemDetailsContainer.js
+++ src/components/ItemDetailsContainer.js
@@ -3,11 +3,10 @@ import { useParams } from "react-router-dom"
 import ItemDetails from "./ItemDetails"
 import {CartContext} from "../context/CartContext"
 import {coleccion} from "../firebase/Firebase"
-import { getDocs} from "firebase/firestore"
+import { getDocs, where, query} from "firebase/firestore"
 
 const ItemDetailsContainer = () => {
 
-    const pedido = getDocs(coleccion)
 
     const { id } = useParams()
 
@@ -16,22 +15,33 @@ const ItemDetailsContainer = () => {
     const [added, setAdded] = useState(false)
 
     const {agregarCarrito} = useContext(CartContext)
+        
     useEffect(() => {
+        
+        let pedido 
+        
+        if(id){
+            const filtro1 = where("categoria","==",id)
+            const filtro2 = where("precio",">",0)
+            const consulta = query(coleccion,filtro1,filtro2)
+             pedido = getDocs(consulta)
+        }else {
+             pedido = getDocs(coleccion)
+        }
 
-       let productos = pedido.docs
-    console.log(productos)
-     if(id){
-        const producto = productos.find(e => e.id === parseInt(id))
-           setProducto(producto)  
-    }
+        pedido
+            .then((resultado)=>{
+                setProducto(resultado.docs.map(doc=>({id : doc.id,...doc.data()})))
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+    },[id])
     
-
-}, [id])
-
-const onAdd = (count) =>{
+    const onAdd = (count) =>{
     agregarCarrito(producto,count);
     setAdded(true);
-}
+    }
      return (
          <div>
              <ItemDetails onAdd={onAdd} producto={producto} added={added}/>
